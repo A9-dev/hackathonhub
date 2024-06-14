19f075b6f82bac53abcf9d4de2274ea9438fe6a4
henry <henrypearson422@gmail.com>
2024-06-15 00:34:57 +0100

    feat: added to info section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,15 +23,44 @@ export default function Home() {
         <h1 className="display-1">{prompt}</h1>
         <p>01:13:21</p>
       </div>
-      <div className="container text-center  border rounded-4 rounded-top-0 bg-primary-subtle p-5">
-        <h1>Welcome to HackathonHub!</h1>
-        <h2>This is a platform for hosting and managing hackathons.</h2>
+      <div className="container border rounded-4 rounded-top-0 bg-primary-subtle p-5">
+        <div className="text-center">
+          <h1>Welcome to HackathonHub!</h1>
+          <h2>This is a platform for hosting and managing hackathons.</h2>
+        </div>
         <br />
-        <p className="text-center m-auto" style={{ width: "50%" }}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores repellat fuga fugiat
-          sequi expedita libero doloremque quos quod pariatur ipsa soluta quis ipsam, illo nemo
-          quasi nulla. Repellat, alias dolores!
-        </p>
+        <div className="m-auto" style={{ width: "70%" }}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Pretium fusce id velit ut tortor. Lacinia
+            at quis risus sed vulputate odio ut enim. Suspendisse faucibus interdum posuere lorem
+            ipsum dolor sit amet. Imperdiet proin fermentum leo vel orci porta. Pharetra massa massa
+            ultricies mi quis hendrerit dolor magna. Laoreet id donec ultrices tincidunt.
+          </p>
+          <b>
+            <ol style={{ listStylePosition: "inside" }}>
+              <li>Thing 1</li>
+              <li>Thing 2</li>
+              <li>Thing 3</li>
+              <li>Thing 4</li>
+              <li>Thing 5</li>
+            </ol>
+          </b>
+          <p>
+            In hendrerit gravida rutrum quisque non tellus. Facilisi nullam vehicula ipsum a arcu
+            cursus vitae. Sit amet facilisis magna etiam. Pellentesque dignissim enim sit amet
+            venenatis urna cursus. Nisi porta lorem mollis aliquam ut porttitor leo a diam. In hac
+            habitasse platea dictumst quisque sagittis purus sit amet. Pellentesque nec nam aliquam
+            sem et tortor consequat id porta. Neque convallis a cras semper. Pretium aenean pharetra
+            magna ac placerat. A iaculis at erat pellentesque adipiscing commodo elit at. Iaculis at
+            erat pellentesque adipiscing commodo elit at imperdiet. Lacus sed turpis tincidunt id
+            aliquet risus feugiat. At risus viverra adipiscing at in tellus integer. Scelerisque
+            mauris pellentesque pulvinar pellentesque habitant morbi. Quisque id diam vel quam
+            elementum. Ornare arcu dui vivamus arcu felis bibendum ut. Ultrices eros in cursus
+            turpis massa tincidunt dui. Tempus urna et pharetra pharetra massa massa. Lobortis
+            scelerisque fermentum dui faucibus in ornare quam viverra orci.
+          </p>
+        </div>
       </div>
     </main>
   );
