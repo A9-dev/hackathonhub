0b3f8bc897765d69c61e3bdd9c64ad39176c6dbe
henry <henrypearson422@gmail.com>
2024-06-14 20:45:44 +0100

    chore: install bootstrap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-import Image from "next/image";
+import "bootstrap/dist/css/bootstrap.min.css";
+// This is how you import bootstrap ^
 
 export default function Home() {
-  return <main>Test</main>;
+  return (
+    <main>
+      <button type="button" className="btn btn-info">
+        Info
+      </button>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+    </main>
+  );
 }
