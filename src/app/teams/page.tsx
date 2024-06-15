a74b77dcff95a5a693842e26589d07590f25fe88
henry <henrypearson422@gmail.com>
2024-06-15 14:18:13 +0100

    feat(teams) added team cards

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -1,3 +1,57 @@
 export default function Home() {
-  return <main>Team Page</main>;
+  return (
+    <main>
+      <div className="container p-5">
+        <div className="container text-center">
+          <div className="row">
+            <div className="col">
+              <div className="card" style={{ width: "18rem" }}>
+                <div className="card-body">
+                  <h5 className="card-title">Example team</h5>
+                  <p className="card-text">Example team description!</p>
+
+                  <button className="btn btn-primary my-1">@ User 1</button>
+                  <br />
+                  <button className="btn btn-primary my-1">@ User 2</button>
+                  <br />
+                  <button className="btn btn-primary my-1">@ User 3</button>
+                  <br />
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card" style={{ width: "18rem" }}>
+                <div className="card-body">
+                  <h5 className="card-title">Example team</h5>
+                  <p className="card-text">Example team description!</p>
+
+                  <button className="btn btn-primary my-1">@ User 1</button>
+                  <br />
+                  <button className="btn btn-primary my-1">@ User 2</button>
+                  <br />
+                  <button className="btn btn-primary my-1">@ User 3</button>
+                  <br />
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card" style={{ width: "18rem" }}>
+                <div className="card-body">
+                  <h5 className="card-title">Example team</h5>
+                  <p className="card-text">Example team description!</p>
+
+                  <button className="btn btn-primary my-1">@ User 1</button>
+                  <br />
+                  <button className="btn btn-primary my-1">@ User 2</button>
+                  <br />
+                  <button className="btn btn-primary my-1">@ User 3</button>
+                  <br />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
